1ce40dddd0805289bb9a6f29ba90e64308e9afd4
Lukasz756 <lgasiorowski0000@gmail.com>
2022-01-16 22:56:01 +0100

    UserInterface class after small remarks and notes.

--- src/main/java/com/sda/weather/Main.java
+++ src/main/java/com/sda/weather/Main.java
@@ -1,12 +1,12 @@
 package com.sda.weather;
 
-import com.sda.weather.userinterface.UserInterface;
+import com.sda.weather.userinterface.userInterface;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Testing"); // todo remove it
-        UserInterface userInterface = new UserInterface();
+
+        userInterface userInterface = new userInterface();
         userInterface.run();
 
     }
--- src/main/java/com/sda/weather/userinterface/userInterface.java
+++ src/main/java/com/sda/weather/userinterface/userInterface.java
@@ -1,19 +1,19 @@
-package com.sda.weather.userinterface; // todo you changed this line but a package name is still the same
+package com.sda.weather.userinterface;
 
 import java.util.Scanner;
 
-public class UserInterface {
+public class userInterface {
 
     public void run() {
-        System.out.println("Weather App is running.");
+        System.out.println("Weather App is running.\n");
         Scanner keyboard = new Scanner(System.in);
 
         while (true) {    //Main UserInterface loop.
             System.out.println("Weather App");
             System.out.println("What do you want to do?");
-            System.out.println("1. Add a localization to database."); // todo just add a localization
-            System.out.println("2. Show database."); // todo change this message, we want to obtain an information about our localizations
-            System.out.println("3. Show weather details from weather service."); // todo it's a weather forecast
+            System.out.println("1. Add a localization.");
+            System.out.println("2. Show localizations.");
+            System.out.println("3. Show weather forecast.");
             System.out.println("4. Close app.");
 
             int decision = keyboard.nextInt();

f4e9c31e6dd1600bb080ddd7e11182537c69d670
Michal Paukszto <michal.paukszto@gmail.com>
2022-01-16 21:05:57 +0100

    small remarks

--- src/main/java/com/sda/weather/Main.java
+++ src/main/java/com/sda/weather/Main.java
@@ -5,7 +5,7 @@ import com.sda.weather.userinterface.UserInterface;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Testing");
+        System.out.println("Testing"); // todo remove it
         UserInterface userInterface = new UserInterface();
         userInterface.run();
 
